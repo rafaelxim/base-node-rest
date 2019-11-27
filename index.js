1c3c9688e6d1892040e26d36662fa98373d73ac4
Rafael Herculano <rafa.lj@hotmail.com>
2019-11-26 21:12:00 -0300

    Setando CORS e incluindo nova rota [movies]

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const winston = require("winston");
 const express = require("express");
 const config = require("config");
 
 const app = express();
-
+require("./startup/cors")(app);
 require("./startup/routes")(app);
 
 const port = process.env.PORT || config.get("port");
