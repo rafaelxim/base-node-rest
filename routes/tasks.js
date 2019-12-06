9f90d8daebbd231551ad3f24f0b3b73805456eb1
Rafael Herculano <rafa.lj@hotmail.com>
2019-12-06 15:37:33 -0300

    Adicionando metodo delete de task

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,37 +5,46 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
 
-    const sort = req.query.order || '-dueDate' ;
+    const sort = req.query.order || '-dueDate';
 
     try {
-        let tasks = await Task.find().sort(sort) ;
+        let tasks = await Task.find().sort(sort);
 
         res.send(tasks);
-    } catch(e){
+    } catch (e) {
         return res.status(400).send(e.message);
     }
-    
+
 });
 
 router.post("/", auth, async (req, res) => {
     // const { error } = validate(req.body);
     // if (error) return res.status(400).send(error.details[0].message);
 
-    try{
+    try {
         let task = new Task({
             name: req.body.name,
-            description : req.body.description ,
-            taskType: req.body.taskType ,
-            dueDate: req.body.dueDate, 
+            description: req.body.description,
+            taskType: req.body.taskType,
+            dueDate: req.body.dueDate,
             poster: req.body.poster
         });
-    
+
         task = await task.save();
-    
+
         res.send(task);
-    } catch(e){
+    } catch (e) {
         return res.status(400).send(e.message);
     }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+    const task = await Task.findByIdAndRemove(req.params.id);
+
+    if (!task)
+        return res.status(404).send("The task with the given ID was not found.");
+
+    res.send(task);
 });
 
 module.exports = router;
